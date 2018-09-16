8fdcecadcbdcc18d15cb3e173e9bf39aa4ed2a18
Marco Angeli <marcoangeli@hotmail.com>
2018-09-15 21:20:28 -0400

    Added emotion_to_phrases and rearranged some stuff

--- image_to_url.py
+++ image_to_url.py
@@ -9,25 +9,44 @@ assert subscription_key
 face_api_url = 'https://canadacentral.api.cognitive.microsoft.com/face/v1.0/detect'
 reddit = praw.Reddit(client_id="M87nRI3kCjaYRw", client_secret="2-sFdzE_TE58cQTMZwLpxZNiC60", user_agent='Hack the North 2018')
 
-
-def emotion_to_url(emotion):
-    emotions = {'anger': None, 'contempt': None, 'disgust': None, 'fear': 'getmotivated', 'happiness': None, 'neutral': None, 'sadness': 'wholesomememes', 'surprise': None, }
-    submissions = []
-
-    for post in reddit.subreddit(emotions[emotion]).top("week", limit=10):
-        submissions.append(post.url)
-    return submissions[random.randint(0, 9)]
-
 def image_to_emotion(image_url):
     headers = {'Ocp-Apim-Subscription-Key': subscription_key}
     params = {'returnFaceAttributes':'emotion'}
     data = {'url': image_url}
     response = requests.post(face_api_url, params=params, headers=headers, json=data)
     faces = response.json()[0]['faceAttributes']['emotion'] # Contains all the emotions with percentages on which is most likely
+    del faces['contempt']
     return max(faces, key=faces.get) #Returns the key with the highest value
 
+def emotion_to_phrases(emotion):
+    phrases =  {'anger': ['Why so angry?'],
+                'contempt': ['Thats a high horse you are on right now.'],
+                'disgust': ['Why so disgusted? Did you look into a mirror?'],
+                'fear': ['Wow you are such'],
+                'happiness': [],
+                'neutral': [],
+                'sadness': [],
+                'surprise': [], }
+
+    return phrases[emotion][random.randint(0, phrases[emotion].length() - 1)]
+
+def emotion_to_url(emotion):
+    emotions = {'anger': None,
+                'contempt': None,
+                'disgust': None,
+                'fear': 'getmotivated',
+                'happiness': None,
+                'neutral': None,
+                'sadness': 'wholesomememes',
+                'surprise': None, }
+
+    submissions = []
+
+    for post in reddit.subreddit(emotions[emotion]).top("week", limit=10):
+        submissions.append(post.url)
+    return submissions[random.randint(0, 9)]
+
 if __name__ == "__main__":
-    image_url = 'https://amp.businessinsider.com/images/506f1690eab8eaef04000004-750-563.jpg'
+    image_url = 'http://www.publicchristian.com/wp-content/uploads/2014/11/Contempt_AS_crop-1123x640.jpg'
     emotion = image_to_emotion(image_url)
     print(emotion)
-    print(emotion_to_url(emotion))
